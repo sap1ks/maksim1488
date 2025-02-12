d5bd610fac8ead9a3bfa1fff9248be491dc466ca
sap1ks <sap1kcsgo@gmail.com>
2025-02-12 09:51:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] omas = new int[n];
+            //for (int i = 0; i < n; i++) 
+            //{
+            //    Console.Write($"omas[{i}]");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for(int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] omas = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write($"omas[{i}]");
+            //    omas[i] = int.Parse(Console.ReadLine());
+
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //int[] omas = new int[] { -1, 5, 0, -3, 2, -2 };
+            //foreach(var el in omas)
+            //{
+            //    Console.Write($"{el}\t");
+            //}    
+            //int[] omas = new int[6];
+            //Random rnd = new Random();
+            //for(int i=0;i<omas.Length; i++)
+            //{
+            //    omas[i] = rnd.Next(-10, 10);
+            //}
+            //foreach (var element in omas)
+            //    Console.Write($"{element}\t");
+            //    Console.Read();
+        }
+    }
+}
